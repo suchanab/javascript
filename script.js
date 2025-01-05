e5e38c27ed75027822eff70e30a69103b809c85c
Suchana Bhandari <bhandarisuchanaaa@gmail.com>
2025-01-05 16:21:09 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,180 @@
+// let x=2;
+// console.log(x**3)
+
+// console.log(5==="5")
+// console.log(5==5)
+// console.log(5!==5)
+// console.log(1<5)
+// console.log(true &&true)
+// console.log(true && false)
+// console.log(true||true)
+
+
+// let fname="suchana"
+// let lname="bhandari"
+// let course="CSIT"
+
+//   ' ' " "  `  `
+// let fullname=`my name is ${fname} ${lname}. im sdfgh ${course}`
+// console.log(fullname)
+
+// let num=5;
+// num+=5
+// console.log(num) //num=num+5
+
+// let age=20;
+// if(age>18)
+//     {
+//     console.log("you are adult")
+//     } else{
+//         console.log("you are child")
+//     }
+
+
+//     let num=10;
+//     if(num>=0){
+//         console.log("the number is positive")
+    
+//     }
+// else{
+//     console.log("the number is negative")
+// }
+
+// let num=4;
+// if(num%2==0){
+//     console.log("the number is even")
+// }
+// else{
+//     console.log("the number is odd")
+// }
+
+
+// let num1=4;
+// let num2=6;
+
+// if(num1>num2){
+//     console.log("the num1 is greater than num2")
+// }else{
+//     console.log(`the ${num2} is greater than ${num1} `)
+// }
+
+
+// let fname="suchana"
+// let lname="bhandari"
+// let fullname=`my name is ${fname}`
+// console.log(fullname)
+
+// let num="3";
+
+// switch(num)
+// {
+//     case "1":
+//     console.log("Number is 1")
+//     break;
+//     case "2":
+//     console.log("Number is 2")
+//     break;
+//     case"3":
+//     console.log("Number is 3")
+//     break;
+//     default:
+//         console.log("number is not given")
+// }
+
+
+// let num1=prompt("enter your age")
+// console.log("Your age is",num1);
+
+
+// let num1=prompt("enter the number");
+
+// switch (num1)
+// {
+    // case "1":
+    // console.log("sunday");
+    // break;
+
+    // case "2":
+    // console.log("monday");
+    // break;
+
+    // case"3":
+    // console.log("tuesday");
+    // break;
+    // case"4":
+    // console.log("wednesday");
+    // break;
+
+    // case"5":
+    // console.log("thursday");
+    // break;
+
+    // case"6":
+    // console.log("friday");
+    // break;
+
+    // case"7":
+    // console.log("saturday");
+    // break;
+    // default:
+    // console.log("not a week");
+// }
+
+// let i=0;
+
+// for(i; i<=10; i++)
+// {
+//     console.log(i);
+//     // console.log("hello");
+// }
+
+
+// let num1=0;
+// while(num1<=10)
+// {
+//     console.log(num1);
+//     num1=num1+1;
+// }
+
+// do{
+//     console.log(num1);
+//     num1++;
+// } while(num1>=10)
+
+
+// function sum()
+// {
+//     console.log("hello this is sum of function")
+//     console.log("hello this is sum of function")
+//     console.log("hello this is sum of function")
+//     console.log("hello this is sum of function")
+
+// }
+// sum();
+
+
+// function fullname(firstname ,secondname)
+// {
+//    let name=`my name is ${firstname} ${secondname}` 
+//    console.log(name)
+// }
+
+// fullname("suchana","bhandari")
+
+
+
+
+// function printsum()
+// {
+//     console.log("sum")
+// }
+// printsum()
+
+
+
+
+let printsum=(num1,num2)=>
+{
+    console.log(num1+num2)
+}
+printsum(3,4)
